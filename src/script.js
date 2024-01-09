fc3476c0bb1811f4367a6e70fc8e45b2a3c1990f
imtiaz <imtiusroshid@gmail.com>
2024-01-09 19:48:29 +0600

    dks

--- src/script.js
+++ src/script.js
@@ -6,3 +6,39 @@ function myFunction() {
         x.style.display = "block";
     }
 } 
+
+function myFunction2() {
+    var x = document.querySelector(".jsDropdown2");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+function toggleModal() {
+    var x = document.querySelector(".jsDropdown3");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+function  myFunction5() {
+    var x = document.querySelector(".jsDropdown5");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+
+function  myFunction6() {
+    var x = document.querySelector(".jsDropdown6");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+} 
+
+
